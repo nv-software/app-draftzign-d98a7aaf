1f3253a364592d68f7127205743fc4115ee94dcf
polivetti <paulo.olivetti@holu.com.br>
2025-02-28 01:02:24 -0300

    fix:material design

--- src/app/container/campain/campain.component.ts
+++ src/app/container/campain/campain.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-campain',
   standalone: true,
   templateUrl: './campain.component.html',
   styleUrl: './campain.component.css',
-  // imports: [MatSlideToggleModule]
+  imports: [MatSlideToggleModule]
 })
 export class CampainComponent {
 
