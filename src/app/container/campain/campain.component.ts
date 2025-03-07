2c766597747a531a9bc14743261db56018f23ca5
polivetti <paulo.olivetti@holu.com.br>
2025-03-06 21:30:02 -0300

    fixes

--- src/app/container/campain/campain.component.ts
+++ src/app/container/campain/campain.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-// import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon'
 @Component({
   selector: 'app-campain',
   standalone: true,
   templateUrl: './campain.component.html',
   styleUrl: './campain.component.css',
-  // imports: [MatIconModule],
+  imports: [MatIconModule],
 })
 export class CampainComponent {
 
