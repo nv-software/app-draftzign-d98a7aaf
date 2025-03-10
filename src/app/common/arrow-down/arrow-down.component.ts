f1de49a6b6d5fbf488883a36dccfd2ccd39a9038
matheus-lemos <matheus.lemos@holu.com.br>
2025-03-10 18:04:11 -0300

    feat: adding scroll

--- src/app/common/arrow-down/arrow-down.component.ts
+++ src/app/common/arrow-down/arrow-down.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-arrow-down',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './arrow-down.component.html',
   styleUrl: './arrow-down.component.css'
 })
 export class ArrowDownComponent {
   scrollDown() {
 
+    const someScroll = window.screen.height - (window.screen.height * 0.3)
+
+    window.scroll({behavior: 'smooth', top: someScroll})
   }
 }
