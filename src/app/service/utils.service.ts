2af4e88856ad0ab8d4725fda55aac73fffcd066d
polivetti <paulo.olivetti@holu.com.br>
2025-03-26 10:37:54 -0300

    update whats number

--- src/app/service/utils.service.ts
+++ src/app/service/utils.service.ts
@@ -14,7 +14,7 @@ export class UtilsService {
       elementId: "cta",
       elementClass: "cta",
     });
-    const phone = "5511993477055"; // +55 11 99347-7055 em formato sem formatação
+    const phone = "5511946838187"; // +55 11 99347-7055 em formato sem formatação
     const encodedMessage = encodeURIComponent(message);
     const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encodedMessage}`;
     window.open(url, "_blank");
